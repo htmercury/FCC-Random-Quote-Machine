17738e2b518980f861ad71ac653a5c4238f0d6c0
Ka Wong <kawong2020@u.northwestern.edu>
2017-12-24 20:43:30 -0600

    Completed random quote machine of LoL quotes

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
-$(document).ready(function() {
-  $("#getMessage").on("click", function() {
-    $.getJSON("https://okrammus.glitch.me/random", function(json) {
-      $(".quote-sign, .message, .name").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-        $(".champion-img").attr("src",json.image);
-        $(".name").html("- " + json.name); $(".message").html(json.quotes[Math.floor(Math.random() * json.quotes.length)]);
+$(document).ready(function () {
+    $(".getMessage").on("click", function () {
+        $.getJSON("https://okrammus.glitch.me/random", function (json) {
+            $(".quote-sign, .message, .name").animate({
+                opacity: 0
+            }, 500,
+                function () {
+                    $(this).animate({
+                        opacity: 1
+                    }, 500);
+                    $(".name").html("- " + json.name);
+                    $(".champion-img").attr("src", json.image);
+                    $(".message").html(json.quotes[Math.floor(Math.random() * json.quotes.length)]);
+                });
         });
-        });
-      
-      
-    // Only change code above this line.
-  });
+        // Change Color
+    });
+    $(".tweet-this").on("click", function () {
+        let quote = $(".message").text();
+        let author = $(".name").text();
+    });
+    $(".tumblr-this").on("click", function () {
+        let quote = $(".message").text();
+        let author = $(".name").text().substring(2);
+        window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=LoLquotes&caption=' + author + '&content=' + quote + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
+    });
 });
